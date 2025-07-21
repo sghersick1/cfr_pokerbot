08a3243818206f82f226e962bbedb6552456d618
sghersick1 <hersicks@gmail.com>
2025-07-20 23:03:17 -0400

    cleaned code

--- RPS.py
+++ RPS.py
@@ -19,46 +19,21 @@ class RPS_Player():
         else: 
             return 2 # scissors
         
-    def counterfactual_reward(self, a1, a2):
-        c_rewards = self.truth_table[a2]
-        for i in range(len(self.regrets)):
+    def counterfactual_reward(self, a2):
+        a1 = self.take_turn()
+        c_rewards = self.truth_table[a2] # get all hypothetical rewards
+        for i in range(len(self.regrets)): # update regrets 
             self.regrets[i] += c_rewards[i]
 
         return self.truth_table[a2][a1]
-        
-    
-
-# action 1 is "our" player
-# action 2 is "opponent"
-def pick_winner(a1, a2):
-    if a1 == a2:
-        return 0
-    elif a1 == 'r': # rock
-        if a2 =='p':
-            return -1
-        else: return 1
-    elif a1 == 'p': # paper
-        if a2 == 's':
-            return -1
-        else: return 1
-    else: # scissors
-        if a2 == 'r':
-            return -1
-        else: return 1
-        
-
-
-
 
 def main():
     bot1 = RPS_Player()
     bot2 = RPS_Player()
 
-    # take turn
-    for i in range(1000000):
-        a1 = bot1.take_turn()
-        a2 = bot2.take_turn()
-        bot1.counterfactual_reward(a1, a2)
+    # bots play each other
+    for i in range(10):
+        bot1.counterfactual_reward(bot2.take_turn())
 
     print(bot1.regrets)
     
